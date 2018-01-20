7a89feec6cf9ff9bd538c1e7f63bf216c7465023
London <360128674@qq.com>
2018-01-21 01:05:46 +0800

    点灯大法

---
+++ hellothings/src/main/java/com/londonx/hellothings/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.londonx.hellothings
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.PeripheralManagerService
+
+/**
+ * Skeleton of an Android Things activity.
+ *
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ *
+ * <pre>{@code
+ * val service = PeripheralManagerService()
+ * val mLedGpio = service.openGpio("BCM6")
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+ * mLedGpio.value = true
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ *
+ */
+class MainActivity : Activity() {
+    private val service = PeripheralManagerService()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        service.gpioList.forEach { log(it) }
+        val outPins = listOf(
+                service.openGpio("IO1").also { it.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW) },
+                service.openGpio("IO2").also { it.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW) },
+                service.openGpio("IO3").also { it.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW) },
+                service.openGpio("IO4").also { it.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW) }
+        )
+        Thread {
+            while (!isDestroyed) {
+                outPins.forEach {
+                    it.value = !it.value
+                    Thread.sleep(100)
+                }
+            }
+            outPins.forEach {
+                it.close()
+            }
+        }.start()
+    }
+}
+
+fun Any.log(string: String) {
+    Log.i(javaClass.simpleName, string)
+}
